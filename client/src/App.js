b42695eb10b2731e1c7a22dbf2700705665dcbda
Danh <danh.nghuu@gmail.com>
2024-03-19 17:33:03 +0700

    update UI betting

--- client/src/App.js
+++ client/src/App.js
@@ -6,58 +6,15 @@ import moment from 'moment';
 import _ from 'lodash';
 import Datepicker from "react-tailwindcss-datepicker";
 
-const getListByDate = async (date) => {
-
-  const options = {
-    method: 'GET',
-    url: 'https://livescore6.p.rapidapi.com/matches/v2/list-by-date',
-    params: {
-      Category: 'basketball',
-      Date: '20240318',
-      Timezone: '-5'
-    },
-    headers: {
-      'X-RapidAPI-Key': 'fbdda89d74msh7b990edc2ea51eep1bcd58jsn73008d56b75f',
-      'X-RapidAPI-Host': 'livescore6.p.rapidapi.com'
-    }
-  };
-
-  try {
-    const response = await axios.request(options);
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-const getMatchResult = async (id) => {
-
-  const options = {
-    method: 'GET',
-    url: 'https://livescore6.p.rapidapi.com/matches/v2/get-scoreboard',
-    params: {
-      Category: 'basketball',
-      Eid: '1057946'
-    },
-    headers: {
-      'X-RapidAPI-Key': 'fbdda89d74msh7b990edc2ea51eep1bcd58jsn73008d56b75f',
-      'X-RapidAPI-Host': 'livescore6.p.rapidapi.com'
-    }
-  };
-
-  try {
-    const response = await axios.request(options);
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-
 function App() {
-  const [dateRange, setDateRange] = useState(20240319);
+
+  const [dateRange, setDateRange] = useState(moment().format('YYYYMMDD').toString());
   const [matchDates, setMatchDate] = useState([]);
   const [betTeams, setBetTeams] = useState({ team1: '', team2: '' });
+  const [value, setValue] = useState({
+    startDate: null,
+    endDate: null
+  });
 
 
   useEffect(() => {
@@ -69,21 +26,17 @@ function App() {
           url: `api/matches?date=${dateRange}`
         };
         const res = await axios.request(options);
-        console.log('res11111:', res);
         setMatchDate([res.data]);
       } catch (error) {
         console.error(error);
       }
     };
     fetchData();
-  }, [dateRange]); // Thêm dateRange vào mảng dependencies
+  }, [dateRange]); // Add dateRange into dependencies
 
   const DateBlock = ({ data }) => {
-    // console.log('data?.Events: ', data);
     return data?.map((match) => {
-      console.log('match.Events: ', match.Events);
       return match.Events.map((event, index) => {
-        console.log('event: ', event);
         return <Match key={index} data={event} index={index} />
       });
     });
@@ -100,8 +53,6 @@ function App() {
     const team1Logo = `https://lsm-static-prod.livescore.com/medium/${data.T1[0].Img}`;
     const team2Logo = `https://lsm-static-prod.livescore.com/medium/${data.T2[0].Img}`;
 
-    console.log('dateStr:', data);
-
     const handleBetClick = () => {
       setBetCardVisible(true);
       setBetTeams({ team1, team2 });
@@ -112,19 +63,17 @@ function App() {
         <div>
           <p>{time}<br />{dateFormatted}</p>
         </div>
-        <div>
-          <div>
-            <p className="text-lg font-semibold">Match {index + 1}</p>
-            <div className="grid grid-cols-3 items-center gap-2">
-              <div className="flex flex-col items-end">
-                <img className='mr-5' src={team1Logo} alt="team1 logo" />
-                <span>{team1}</span>
-              </div>
-              <span>vs</span>
-              <div className="flex flex-col items-start">
-                <img className='ml-5' src={team2Logo} alt="team2 logo" />
-                <span>{team2}</span>
-              </div>
+        <div className='flex flex-col grow items-center'>
+          <p className="text-lg font-semibold">Match {index + 1}</p>
+          <div className="grid grid-cols-[1fr_50px_1fr] items-center w-[100%]">
+            <div className="flex flex-col items-center">
+              <img className='h-[48px] w-[48px]' src={team1Logo} alt="team1 logo" />
+              <span>{team1}</span>
+            </div>
+            <span>vs</span>
+            <div className="flex flex-col items-center">
+              <img className='h-[48px] w-[48px]' src={team2Logo} alt="team2 logo" />
+              <span>{team2}</span>
             </div>
           </div>
         </div>
@@ -140,7 +89,8 @@ function App() {
   const BetCard = (props) => {
     const { team1, team2 } = props;
     return (
-      <div className="flex flex-col items-start justify-start border h-80 p-4 mt-20 m-1 w-3/12 space-y-2 bg-white shadow-lg rounded-lg">          <div className="text-lg font-semibold">Choose a team:</div>
+      <div className="flex flex-col items-start justify-start border h-80 p-4 mt-20 m-1 w-3/12 space-y-2 bg-white shadow-lg rounded-lg">
+        <div className="text-lg font-semibold">Choose a team:</div>
         <div className="space-y-2">
           <label className="flex items-center">
             <input type="radio" value="team1" name="team" className="mr-2" /> {team1}
@@ -158,13 +108,7 @@ function App() {
     );
   };
 
-  const [value, setValue] = useState({
-    startDate: null,
-    endDate: null
-  });
-
   const handleValueChange = (newValue) => {
-    console.log("newValue:", newValue);
     setValue(newValue);
     const selectedDate = moment(newValue.startDate).format('YYYYMMDD');
     setDateRange(selectedDate);
@@ -183,8 +127,8 @@ function App() {
   const [isBetCardVisible, setBetCardVisible] = useState(false);
 
   return (
-    <div className="App flex">
-      <div className="flex items-start justify-start">
+    <div className="App flex px-[150px] py-0 justify-around">
+      <div className="flex items-start justify-start absolute top-0 left-0">
         <div className="flex justify-left space-x-4 p-4">
           <input type="checkbox" id="testMode" name="testMode" value="TestMode" className="form-checkbox h-5 w-5 text-blue-600" />
           <label htmlFor="testMode" className="ml-2 text-gray-700 text-sm">Test mode</label>
@@ -195,8 +139,8 @@ function App() {
           Wallet
         </button>
       </div>
-      <div className="w-7/12">
-        <div className="flex flex-col max-w-5xl mx-auto">
+      <div className={`${isBetCardVisible ? 'w-[70%]' : 'w-[100%]'} transform transition-all ease-linear`}>
+        <div className="flex flex-col">
           <div className="flex justify-left space-x-4 p-4">
             <button onClick={fetchTodayData} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
               Today
